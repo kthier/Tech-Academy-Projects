2f4e8a7b67a4c248b686fca27b91a36b82c17f08
Kaori Thier <kaori.thier@gmail.com>
2017-12-02 15:03:54 -0800

    Challenge 9 - Postage Calculator

---
+++ Challenge  9 - Postage Calculator/Challenge  9 - Postage Calculator/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Challenge__9___Postage_Calculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <strong><span class="auto-style1">Postage Calculator<br />
+        </span></strong>
+        <br />
+        <br />
+        Width:&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="widthTextBox_TextChanged"></asp:TextBox>
+        <br />
+        <br />
+        Height:&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="heightTextBox_TextChanged"></asp:TextBox>
+        <br />
+        <br />
+        Length:&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="lengthTextBox_TextChanged"></asp:TextBox>
+        <br />
+        <br />
+        <asp:RadioButton ID="groundRadioButton" runat="server" AutoPostBack="True" Checked="True" GroupName="shipping" OnCheckedChanged="groundRadioButton_CheckedChanged" Text="Ground" />
+        <br />
+        <asp:RadioButton ID="airRadioButton" runat="server" AutoPostBack="True" GroupName="shipping" OnCheckedChanged="airRadioButton_CheckedChanged" Text="Air" />
+        <br />
+        <asp:RadioButton ID="nextDayRadioButton" runat="server" AutoPostBack="True" GroupName="shipping" OnCheckedChanged="nextDayRadioButton_CheckedChanged" Text="Next Day" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Challenge  9 - Postage Calculator/Challenge  9 - Postage Calculator/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Challenge__9___Postage_Calculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            calculateShipping(1, 1);
+        }
+
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            calculateShipping(1, 1);
+        }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            calculateShipping(1, 1);
+        }
+
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateShipping(1,1);
+        }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateShipping(1, 1);
+        }
+
+        protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateShipping(1, 1); 
+        }
+
+        private void calculateShipping(double width, double height, double length = 1)
+        {
+            if (widthTextBox.Text.Trim().Length == 0)
+                return;
+            if (heightTextBox.Text.Trim().Length == 0)
+                return;
+
+            width = 0;
+            if (!Double.TryParse(widthTextBox.Text, out width))
+                return;
+
+            height = 0;
+            if (!Double.TryParse(heightTextBox.Text, out height))
+                return;
+
+            length = 0; //my optional
+            if (!Double.TryParse(lengthTextBox.Text, out length))
+            { length = 1; }
+
+            double shipping = 0.00;
+
+            if (groundRadioButton.Checked) shipping = width * height * length * .15;
+            if (airRadioButton.Checked) shipping = width * height * length * .25;
+            if (nextDayRadioButton.Checked) shipping = width * height * length * .45;
+
+            resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", shipping);
+        }
+    }
+}
