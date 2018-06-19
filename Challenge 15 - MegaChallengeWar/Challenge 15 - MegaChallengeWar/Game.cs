9dd4ee345f936fa9f92f3115add7ad041b1ffd90
Kaori Thier <30737209+kthier@users.noreply.github.com>
2018-06-19 12:55:56 -0700

    Challenge 14 Revised

--- Challenge 15 - MegaChallengeWar/Challenge 15 - MegaChallengeWar/Game.cs
+++ Challenge 15 - MegaChallengeWar/Challenge 15 - MegaChallengeWar/Game.cs
@@ -9,18 +9,44 @@ namespace Challenge_15___MegaChallengeWar
     {
         private Player _player1;
         private Player _player2;
-    
-    
+
         public Game(string player1Name, string player2Name)
         {
            _player1 = new Player() { Name = player1Name };
-           _player2 = new Player() { Name = player2Name };         
+           _player2 = new Player() { Name = player2Name };          
         }
 
         public string Play()
         {
-        Deck deck = new Deck();
-        return  deck.Deal(_player1, _player2);
+            Deck deck = new Deck();
+            string result = "<h1>Dealing Cards...</h1>";
+            result += deck.Deal(_player1, _player2);
+
+            result += "<h2>Begin Battle...</h2>";
+
+            int round = 0;
+            while (_player1.Cards.Count !=0 && _player2.Cards.Count !=0)
+            {
+                Battle battle = new Battle();
+                result += battle.PerformBattle(_player1, _player2);
+
+                round++;
+                if (round > 20)
+                break;
+            }
+            result += determineWinner();
+            return result;
+        }
+        
+        private string determineWinner()
+        {
+            string result = "";
+            if (_player1.Cards.Count > _player2.Cards.Count)
+                result+= "<br><br><span>Player One Wins! </span>";
+            else result+= "<br><br><span>Player Two Wins! </span>";
+
+            result += "<br><br>Player One scored " + _player1.Cards.Count + " and Player Two scored " + _player2.Cards.Count + "!";
+            return result;
         }
         
     }
